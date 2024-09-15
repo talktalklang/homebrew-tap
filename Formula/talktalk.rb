d6111588e59a019c332a83b1e80bf59cf37895b9
Joshua Peek <josh@users.noreply.github.com>
2024-09-14 21:13:03 -0700

    Update homepage url

--- Formula/talktalk.rb
+++ Formula/talktalk.rb
@@ -1,6 +1,6 @@
 class TalkTalk < Formula
   desc "The TalkTalk programming language"
-  homepage "https://github.com/nakajima/talktalk"
+  homepage "https://github.com/talktalklang/talktalk"
   url "https://github.com/talktalklang/talktalk/releases/download/0.0.1/talk.zip"
   sha256 "0fe0956fd3c3c38dc96d7cfaec503df3b610e03f15fdf3b70f0763e291020b6a"
   license "BSD-3-Clause"
