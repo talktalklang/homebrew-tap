15a307e9b68d814eecadbc13e7912c31be6e2c16
Pat Nakajima <patnakajima@gmail.com>
2024-09-15 10:52:02 -0700

    Update talktalk.rb

--- Formula/talktalk.rb
+++ Formula/talktalk.rb
@@ -2,11 +2,18 @@ class Talktalk < Formula
   desc "The TalkTalk programming language"
   homepage "https://github.com/talktalklang/talktalk"
   url "https://github.com/talktalklang/talktalk/releases/download/0.0.1/TalkTalk.zip"
-  sha256 "4cecb5c4d4988079a0d7fb3099c757306f7a6435ea9c4dd544d452b7b75742a9"
+  sha256 "150673aab001a9bf8466ac3e4e84b18056f0ab479b378bd9f8f1416c9b0d3215"
   license "BSD-3-Clause"
 
   def install
     bin.install "talk"
-    prefix.install "TalkTalk_TalkTalkCore.bundle"
+    libexec.install "TalkTalk_TalkTalkCore.bundle"
+
+    # Create a wrapper script to set the environment variable for the bundle path
+    (bin/"talk").write <<~EOS
+      #!/bin/bash
+      export TALKTALK_BUNDLE_PATH=#{libexec}/TalkTalk_TalkTalkCore.bundle
+      exec "#{bin}/talk" "$@"
+    EOS
   end
 end
